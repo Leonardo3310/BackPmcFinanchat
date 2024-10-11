32cfec73e3d24c21bfc3c81624abe00e727b3021
Francois Morales <f.moraless@uniandes.edu.co>
2024-10-11 00:33:34 -0500

    End Point 1 terminado

--- Aplicacion/main.py
+++ Aplicacion/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import predicciones as pred
+from typing import List
 
 app = FastAPI()
+
+class Opinion(BaseModel):
+    opinion: str
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+@app.post("/prediccion/")
+def hacer_prediccion(opiniones: List[Opinion]):
+    respuesta = pred.clasificacion(opiniones)
+    return respuesta
--- Aplicacion/predicciones.py
+++ Aplicacion/predicciones.py
@@ -4,16 +4,34 @@ import pandas as pd
 from Dependencies import preprocessing_text, Transformer_Representacion_Seleccion 
 
 #Cargar el modelo
-modelo = joblib.load('Aplicacion/Modelos/pipelon.joblib')
+modelo = joblib.load("Aplicacion/pipelon.joblib")
 
 #Funcion para clasificar opiniones (End-point #1)
 def clasificacion(opiniones):
+
     tamano = len(opiniones)
     predicciones = []
+
     for i in range(tamano):
+
         opinion = opiniones[i]
         opinion_df = pd.DataFrame({'Textos_espanol': [opinion]})
-        prediccion = modelo.predict(opinion_df)
+        clase_predicha  = int(modelo.predict(opinion_df)[0])
+        probabilidades = modelo.predict_proba(opinion_df)
+
+        prob_ods3 = str(round(float(probabilidades[0][0])*100,2))
+        prob_ods4 = str(round(float(probabilidades[0][1])*100,2))
+        prob_ods5 = str(round(float(probabilidades[0][2])*100,2))
+
+        prediccion = {
+            "# Opinion": i+1,
+            "clase_predicha": clase_predicha,
+            "probabilidades": {
+                "ODS 3": prob_ods3+' %',
+                "ODS 4": prob_ods4+' %',
+                "ODS 5": prob_ods5+' %'
+            }
+        }
         predicciones.append(prediccion)
     return predicciones
 
