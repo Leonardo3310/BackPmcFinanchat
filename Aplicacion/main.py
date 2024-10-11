3fd50f4dbcb3455c087ac2f587f63ed80f7706d4
Francois Morales <f.moraless@uniandes.edu.co>
2024-10-11 13:25:22 -0500

    End point 2

--- Aplicacion/main.py
+++ Aplicacion/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 import predicciones as pred
 from typing import List
+from io import BytesIO
 
 app = FastAPI()
 
@@ -16,3 +17,21 @@ def index():
 def hacer_prediccion(opiniones: List[Opinion]):
     respuesta = pred.clasificacion(opiniones)
     return respuesta
+
+
+@app.post("/reentrenar/")
+async def reentrenar_modelo(file: UploadFile = File(...)):
+    if file.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        raise HTTPException(status_code=400, detail="File format not supported")
+
+    try:
+        contents = await file.read()
+        excel_data = BytesIO(contents)
+        df_retrain = pred.unir_datos(excel_data)
+
+        matrics = pred.reentrenar_modelo(pred.modelo, df_retrain)
+
+        return matrics
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error durante el reentrenamiento: {str(e)}")
--- Aplicacion/predicciones.py
+++ Aplicacion/predicciones.py
@@ -1,11 +1,13 @@
 import sklearn as sk
 import joblib
 import pandas as pd
+from io import BytesIO
+from sklearn.model_selection import train_test_split
 from Dependencies import preprocessing_text, Transformer_Representacion_Seleccion 
+from sklearn.metrics import classification_report
 
 #Cargar el modelo
-modelo = joblib.load("Aplicacion/Modelos/pipelon.joblib")
-#modelo_retrain = joblib.load("Aplicacion/Modelos/pipelon-retrain.joblib")
+modelo = joblib.load("Aplicacion/pipelon.joblib")
 
 #Funcion para clasificar opiniones (End-point #1)
 def clasificacion(opiniones):
@@ -37,4 +39,33 @@ def clasificacion(opiniones):
     return predicciones
 
 
-#Funcion reentrenar el modelo (End-point #2)
+#Funcion para unir los datos de ambos archivos para el reentrenamiento del modelo
+def unir_datos(file: BytesIO):
+    archivo1 = pd.read_excel('Aplicacion/Data/ODScat_345.xlsx')
+    archivo2 = pd.read_excel(file)
+    data = pd.concat([archivo1, archivo2], ignore_index=True)
+    
+    return data
+
+df = pd.read_excel('Aplicacion/Data/ODScat_345.xlsx')
+
+#Funcion para reentrenar el modelo (End-point #2)
+def reentrenar_modelo(model, data):
+    data_t, data_v = train_test_split(data, test_size=0.2, random_state=0)
+
+    y_train = data_t['sdg']
+    x_train = data_t.drop(columns='sdg')
+
+    x_val = data_v.drop(columns='sdg')
+    y_val = data_v['sdg']
+
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_val)
+
+    metrics = classification_report(y_val, y_pred)
+    joblib.dump(model, 'Aplicacion/pipelon.joblib')
+
+    return metrics
+
+re = reentrenar_modelo(modelo, df)
+print(re)
