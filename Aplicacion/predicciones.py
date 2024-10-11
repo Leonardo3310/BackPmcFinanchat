eea06a38a46ef1ba9c98e8fb9890167f97b677af
Francois Morales <f.moraless@uniandes.edu.co>
2024-10-11 16:11:47 -0500

    API Final

--- Aplicacion/predicciones.py
+++ Aplicacion/predicciones.py
@@ -49,7 +49,7 @@ def unir_datos(file: BytesIO):
     
     return data
 
-#df = pd.read_excel('ODScat_345.xlsx')
+df = pd.read_excel('ODScat_345.xlsx')
 
 #Funcion para reentrenar el modelo (End-point #2)
 def reentrenar_modelo(model, data):
@@ -64,10 +64,30 @@ def reentrenar_modelo(model, data):
     model.fit(x_train, y_train)
     y_pred = model.predict(x_val)
 
-    metrics = classification_report(y_val, y_pred)
+    metrics = classification_report(y_val, y_pred, output_dict=True)
+
+    ods_3 = {
+        'precision': round(metrics['3']['precision'],2),
+        'recall': round(metrics['3']['recall'],2),
+        'f1-score': round(metrics['3']['f1-score'],2)
+    }
+    ods_4 = {
+        'precision': round(metrics['4']['precision'],2),
+        'recall': round(metrics['4']['recall'],2),
+        'f1-score': round(metrics['4']['f1-score'],2)
+    }
+    ods_5 = {
+        'precision': round(metrics['5']['precision'],2),
+        'recall': round(metrics['5']['recall'],2),
+        'f1-score': round(metrics['5']['f1-score'],2)
+    }
+
     joblib.dump(model, 'pipelon.joblib')
 
-    return metrics
+    metrics_dicc = {
+        1: ods_3,
+        2: ods_4,
+        3: ods_5
+    }
 
-#re = reentrenar_modelo(modelo, df)
-#print(re)
+    return metrics_dicc
