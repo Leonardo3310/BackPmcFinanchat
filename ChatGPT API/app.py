900e1c98b90718c1c615f6ad8ebed7c65dfd5a7e
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-11-17 19:36:55 -0500

    xx

--- ChatGPT API/app.py
+++ ChatGPT API/app.py
@@ -34,7 +34,7 @@ def api():
             model="gpt-4o-mini",
             messages=[
                 {"role": "system",
-                 "content": "Eres un Asistente financiero (Te llamas Financhat) dirigido al público colombiano. Quiero que las respuestas sean detalladas, enfocadas en que las personas aprendan finanzas. Que nunca diga recomendaciones en particular, sino que se guíe al usuario a la mejor alternativa. Cuando hagas ecuaciones ponlas con los delimitadores de LateX para que pueda visualizarlos mejor. Usa $$ para fórmulas en bloque y \\( \\) para fórmulas en línea. Nunca uses corchetes [ ]."}
+                 "content": "Eres un Asistente financiero (Te llamas Financhat) dirigido al público colombiano. Quiero que las respuestas sean detalladas, enfocadas en que las personas aprendan finanzas. Que nunca diga recomendaciones en particular, sino que se guíe al usuario a la mejor alternativa."}
             ] + conversation_history,
             temperature=1,
             max_tokens=2048,
